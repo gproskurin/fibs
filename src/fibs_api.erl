2164f5385a4b266c414bc55a5f57f7551d3c9708
Gennady Proskurin <gpr@mail.ru>
2024-03-17 14:37:03 +0000

    Basic blacklist api

--- src/fibs_api.erl
+++ src/fibs_api.erl
@@ -1,16 +1,33 @@
 -module(fibs_api).
 
-
 -export([
-    generate_rev/2
+    generate_rev/2,
+
+    blacklist_add/1,
+    blacklist_remove/1,
+    blacklist_has/1
 ]).
 
+-include("include/fibs.hrl").
 
+
+-spec generate_rev(
+        NumCount :: integer(),
+        Prev :: undefined | [integer()]
+    ) -> [integer()].
+% Generate sequence of fibonacci numbers with specified length.
+% If optional list of previous numbers is specified, continue generation of
+% sequence from those previous numbers.
+%
+% Handle special cases for N=1,2, pass to generic recursive function for N>2
+% with pre-generated accumulator of first two numbers
 generate_rev(1, undefined = _Prev) ->
     [0];
+
 generate_rev(2, undefined = _Prev) ->
     [1, 0];
-generate_rev(N, undefined = _Prev) when N > 2 ->
+
+generate_rev(N, undefined = _Prev) ->
     Acc = [1, 0],
     generate_rev_impl(N-2, Acc);
 
@@ -25,10 +42,26 @@ generate_rev(N, [A, B] = _Prev) ->
     generate_rev_impl(N-2, Acc).
 
 
+% Generic function to generate fibonacci sequence using accumulator of already generated numbers.
+% Requires the accumulator to have at least 2 numbers
 generate_rev_impl(0, Acc) ->
     Acc;
 generate_rev_impl(N, [A, B | _] = Acc) ->
     generate_rev_impl(N-1, [A+B | Acc]).
+
+
+% blacklist functions
+blacklist_add(X) when is_integer(X) ->
+    true = ets:insert(?FIBS_BLACKLIST_ETS, {X}).
+
+blacklist_remove(X) when is_integer(X) ->
+    true = ets:delete(?FIBS_BLACKLIST_ETS, X).
+
+blacklist_has(X) when is_integer(X) ->
+    case ets:lookup(?FIBS_BLACKLIST_ETS, X) of
+        [_] -> true;
+        [] -> false
+    end.
 
 
 -ifdef(TEST).
@@ -54,6 +87,43 @@ fib_test() ->
     ?assertEqual(20000, length(Fbig2)),
 
     ?assertEqual([41,25,16,9], generate_rev(4, [7,2])).
+
+
+blacklist_table_test_() ->
+    {
+        setup,
+        fun() -> fibs_app:blacklist_table_create() end,
+        fun(_) -> fibs_app:blacklist_table_destroy() end,
+        [
+            fun test_blacklist_table/0
+        ]
+    }.
+
+
+test_blacklist_table() ->
+    ?assertNot(blacklist_has(123)),
+    ?assertNot(blacklist_has(456)),
+
+    blacklist_add(123),
+    ?assert(blacklist_has(123)),
+    ?assertNot(blacklist_has(456)),
+
+    blacklist_add(456),
+    ?assert(blacklist_has(123)),
+    ?assert(blacklist_has(456)),
+
+    blacklist_remove(123),
+    ?assertNot(blacklist_has(123)),
+    ?assert(blacklist_has(456)),
+
+    % repeat, check blacklist is still usable
+    blacklist_remove(123),
+    ?assertNot(blacklist_has(123)),
+    ?assert(blacklist_has(456)),
+
+    blacklist_remove(456),
+    ?assertNot(blacklist_has(123)),
+    ?assertNot(blacklist_has(456)).
 
 
 -endif.
--- src/fibs_app.erl
+++ src/fibs_app.erl
@@ -9,8 +9,15 @@
 
 -export([start/2, stop/1]).
 
+-ifdef(TEST).
+-export([blacklist_table_create/0, blacklist_table_destroy/0]).
+-endif.
+
+-include("include/fibs.hrl").
+
 
 start(_StartType, _StartArgs) ->
+    blacklist_table_create(),
     Dispatch = cowboy_router:compile([
         {'_', [
             {<<"/fibs/generate">>, fibs_http_handler, []}
@@ -24,6 +31,15 @@ start(_StartType, _StartArgs) ->
     fibs_sup:start_link().
 
 stop(_State) ->
+    blacklist_table_destroy(),
     ok.
+
+
+blacklist_table_create() ->
+    _Table = ets:new(?FIBS_BLACKLIST_ETS, [set, named_table, public, {keypos,1}]).
+
+blacklist_table_destroy() ->
+    true = ets:delete(?FIBS_BLACKLIST_ETS).
+
 
 %% internal functions
